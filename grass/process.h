e2f0030af9601eb1ac32d76791108b3fb2f7c6be
Anmol-Saluja <anmol24085@iiitd.ac.in>
2025-11-23 23:35:01 +0530

    Update process.h

--- grass/process.h
+++ grass/process.h
@@ -17,14 +17,26 @@ enum proc_status {
 #define SAVED_REGISTER_SIZE SAVED_REGISTER_NUM * 4
 #define SAVED_REGISTER_ADDR (void*)(EGOS_STACK_TOP - SAVED_REGISTER_SIZE)
 
+//MLFQ Scheduler Constants 
+#define MLFQ_NLEVELS          5
+#define MLFQ_RESET_PERIOD     10000000         // 10 sec
+#define MLFQ_LEVEL_RUNTIME(x) (x + 1) * 100000 //100ms for level 0 
+
 struct process {
     int pid;
     struct syscall syscall;
     enum proc_status status;
     uint mepc, saved_registers[SAVED_REGISTER_NUM];
     /* Student's code goes here (Preemptive Scheduler | System Call). */
-
     /* Add new fields for lifecycle statistics, MLFQ or process sleep. */
+    ulonglong creation_time;      /* Time when process was created */
+    ulonglong first_schedule_time; /* Time when process was first scheduled */
+    ulonglong total_cpu_time;     /* Total accumulated CPU time */
+    uint timer_interrupt_count;   /* Number of timer interrupts encountered */
+
+    uint mlfq_level;              /* Current MLFQ queue level (0-4) */
+    ulonglong level_runtime;      /* Runtime accumulated at current level */
+    ulonglong last_schedule_time; /* Last time this process was scheduled */
 
     /* Student's code ends here. */
 };
